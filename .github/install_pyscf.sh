46df98714c3d70082bd6ae461bc7735ca786de3e
Eric Berquist <eric.berquist@gmail.com>
2017-07-11 11:03:24 -0400

    Disable caching pyscf while testing Psi4 install.
    Going to use a conda package anyway...

--- .github/install_pyscf.sh
+++ .github/install_pyscf.sh
@@ -3,17 +3,18 @@ set -e
 pip install scipy h5py
 remote_repo="https://github.com/sunqm/pyscf.git"
 pyscf_dir="$HOME/pyscf"
-if [ ! -d "$pyscf_dir" ]; then
-    git clone $remote_repo "$HOME"/pyscf
-elif [ ! -d "$pyscf_dir/.git" ]; then
-    cd "$pyscf_dir"
-    git init
-    git remote add origin $remote_repo
-    git pull
-else
-    cd "$pyscf_dir"
-    git pull
-fi
+# if [ ! -d "$pyscf_dir" ]; then
+#     git clone $remote_repo "$HOME"/pyscf
+# elif [ ! -d "$pyscf_dir/.git" ]; then
+#     cd "$pyscf_dir"
+#     git init
+#     git remote add origin $remote_repo
+#     git pull
+# else
+#     cd "$pyscf_dir"
+#     git pull
+# fi
+git clone $remote_repo "$HOME"/pyscf
 mkdir -p "$pyscf_dir"/lib/build
 cd "$pyscf_dir"/lib/build || exit
 cmake ..
