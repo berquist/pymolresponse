4d1ea3d91d64382d04d726d92efb28fd6257d0d3
Eric Berquist <eric.berquist@gmail.com>
2018-12-15 21:05:45 -0500

    Add files generated by the MolSSI CMS cookiecutter
    https://github.com/MolSSI/cookiecutter-cms/commit/79ee84559f4566ef53c217f19e79a8c254ba73c5

--- pyresponse/__init__.py
+++ pyresponse/__init__.py
@@ -14,3 +14,10 @@ from . import operators
 from . import optrot
 from . import td
 from . import utils
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-import setuptools
+"""
+pyresponse
+Molecular frequency-dependent response properties for arbitrary operators
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
 
 
-def setup_pyresponse():
-
-    setuptools.setup(
-        name="pyresponse",
-        version="0.1alpha",
-        packages=setuptools.find_packages(exclude=["*test*"]),
-
-        install_requires=[
-            "numpy", "scipy", "cclib", "periodictable",
-        ],
-
-        # metadata
-        author="Eric Berquist",
-        maintainer="Eric Berquist",
-        # TODO read this and long_description from README.md
-        description="Molecular frequency-dependent response properties for arbitrary operators",
-        license="BSD 3-Clause License",
-        url="https://github.com/berquist/pyresponse",
-        project_urls={
-            "Documentation": "https://berquist.github.io/pyresponse_docs/",
-        },
-    )
-
-
-if __name__ == "__main__":
-    setup_pyresponse()
+setup(
+    name="pyresponse",
+    author="Eric Berquist",
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='BSD-3-Clause',
+    packages=find_packages(exclude=["*test*"]),
+    url="https://github.com/berquist/pyresponse",
+    python_requires=">=3.6",
+    # install_requires=[
+    #     "numpy", "scipy", "cclib", "periodictable",
+    # ],
+    project_urls={
+        "Documentation": "https://berquist.github.io/pyresponse_docs/",
+    },
+)
