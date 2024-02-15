0488d3e51b154e3ccebb3d3a4c5d1e78834fbfcd
Eric Berquist <eric.berquist@gmail.com>
2024-02-15 08:50:02 -0500

    continue move away from versioneer

--- pyresponse/__init__.py
+++ pyresponse/__init__.py
@@ -1,11 +1,3 @@
-"""
-isort:skip_file
-"""
+"""Molecular frequency-dependent response properties for arbitrary operators"""
 
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+from ._version import __version__
