66e128d9243515ea6e43d866cb6947b015cffedb
Eric Berquist <eric.berquist@gmail.com>
2024-02-15 01:13:47 -0500

    start defining project in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
-"""
-pyresponse
-Molecular frequency-dependent response properties for arbitrary operators
-"""
 from pathlib import Path
 from typing import List, Sequence
 
 from setuptools import find_packages, setup
 
 import versioneer
-
-short_description = __doc__.split("\n")
-
-try:
-    with open("README.md", "r", encoding="utf-8") as handle:
-        long_description = handle.read()
-except FileNotFoundError:
-    long_description = "\n".join(short_description[2:])
 
 
 def _get_files_recursive(top: Path) -> Sequence[Path]:
@@ -41,18 +29,10 @@ def _get_package_data() -> Sequence[Path]:
 
 if __name__ == "__main__":
     setup(
-        name="pyresponse",
         author="Eric Berquist",
-        description=short_description[0],
-        long_description=long_description,
-        long_description_content_type="text/markdown",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
-        license="BSD-3-Clause",
         packages=find_packages(exclude=["*test*"]),
         package_data={"pyresponse": tuple(str(f) for f in _get_package_data())},
-        url="https://github.com/berquist/pyresponse",
-        python_requires=">=3.6",
-        install_requires=["attrs", "cclib", "periodictable"],
         project_urls={"Documentation": "https://berquist.github.io/pyresponse_docs/"},
     )
