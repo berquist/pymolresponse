fa75814cfa9d9b62f3d2285481fdec2fe52b21b6
Eric Berquist <eric.berquist@gmail.com>
2024-07-20 11:32:31 -0400

    initial pixi config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Example C++ development environment for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+      in
+      {
+        devShells = forAllSystems ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              pixi
+            ];
+          };
+        });
+      };
+}
